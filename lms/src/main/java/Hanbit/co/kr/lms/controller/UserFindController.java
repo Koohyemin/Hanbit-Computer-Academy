b498527c82d22b84a78700043c8b31a590c6b44e
yujin981126 <yujin6612@gmail.com>
2022-06-22 14:43:00 +0900

    14:41 [김유진,김하늬] ID찾기 컨트롤러 간소화 및 findId.jsp파일 css 작업

--- lms/src/main/java/Hanbit/co/kr/lms/controller/UserFindController.java
+++ lms/src/main/java/Hanbit/co/kr/lms/controller/UserFindController.java
@@ -15,33 +15,60 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 public class UserFindController {
 	@Autowired private UserFindService userFindService;
-	//학생id찾기 get
+	
+	//id찾기 get
 	@GetMapping("/user/findId")
-	public String findId() {
+	public String findId(Model model
+			,@RequestParam(name = "role",defaultValue = "student")String role) {
+		
+		log.debug(CF.SWB+"[LoginController GetMapping role]"+ role+CF.RESET);
+	
+		model.addAttribute("role",role);
+
 		return "user/findId"; //포워딩
 	}
-	//학생id찾기 post
+	
+	//id찾기 post
 	@PostMapping("/user/findId")
-	public String findId(Model model,
-			@RequestParam(name="studentName") String studentName
-			,@RequestParam(name="studentPhone") String studentPhone) {
+	public String studentSearchId(Model model,
+			@RequestParam(name="name") String name
+			,@RequestParam(name="phone") String phone
+			,@RequestParam(name = "role",defaultValue = "student")String role) {
 		
 		//studentName + studentPhone 값 찾기
-		log.debug( CF.KHN +"[UserController PostMapping studentSearchId studentName]:  "+studentName+ CF.RESET);
-		log.debug( CF.KHN +"[UserController PostMapping studentSearchId studentPhone]:  "+studentPhone+ CF.RESET);
+		log.debug( CF.KHN +"[UserController PostMapping studentSearchId studentName]:  "+name+ CF.RESET);
+		log.debug( CF.KHN +"[UserController PostMapping studentSearchId studentPhone]:  "+phone+ CF.RESET);
+		log.debug( CF.KHN +"[UserController PostMapping studentSearchId role]:  "+role+ CF.RESET);
 		//문자열타입 studentId 서비스에서 호출
 		
-		String studentId = userFindService.studentFindId(studentName, studentPhone);
-		//디버깅
-		log.debug( CF.KHN +"[UserController PostMapping studentSearchId studendId]:  "+studentId+ CF.RESET);
+		String Id = null;
+		// 학생이라면
+		if(role.equals("student")){
+			Id = userFindService.studentFindId(name, phone);
+			//디버깅
+			log.debug( CF.KHN +"[UserController PostMapping studentSearchId Id]:  "+ Id+ CF.RESET);
+		}
 		
-		if(studentId == null) { //studentId가 널값이라면
+		// 강사라면
+		if(role.equals("teacher")){
+			Id = userFindService.teacherFindId(name, phone);
+			//디버깅
+			log.debug( CF.KHN +"[UserController PostMapping studentSearchId Id]:  "+ Id+ CF.RESET);
+		}
+		
+		// 운영자라면
+		if(role.equals("manager")){
+			Id = userFindService.managerFindId(name, phone);
+			//디버깅
+			log.debug( CF.KHN +"[UserController PostMapping studentSearchId Id]:  "+ Id+ CF.RESET);
+		}
+		if(Id == null) { //studentId가 널값이라면
 			model.addAttribute("check", 1); //모델값1로 넘김
 		} else {
 			model.addAttribute("check", 0); //모델값 0으로 넘김
-			model.addAttribute("studentId", studentId); //모델값 studentId도 함께 넘김
+			model.addAttribute("Id", Id); //모델값 studentId도 함께 넘김
 		}
-		return "user/findId";
+		return "/user/findId"; 
 	}
 	
 	//학생pw찾기 get
@@ -49,7 +76,7 @@ public class UserFindController {
 	public String findPw() {
 		return "user/findPw"; //포워딩
 	}
-	
+
 	//학생pw찾기 post
 	@PostMapping("/user/findPw")
 	public String findPw(Model model
@@ -75,63 +102,5 @@ public class UserFindController {
 			model.addAttribute("studentPw", studentPw); //studentPw 넘김
 		}
 		return "user/findPw";
-	}
-	
-	//강사ID찾기 get
-	@GetMapping("/user/teacherSearchId")
-	public String teacherSearchId() {
-		return "user/findId"; //포워딩
-	}
-	
-	//강사ID찾기 post
-	@PostMapping("/user/teacherSearchId")
-	public String teacherSearchId(Model model
-			,@RequestParam(name="teacherName") String teacherName
-			,@RequestParam(name="teacherPhone") String teacherPhone) {
-		
-		// teacherName + teacherPhone 값 찾기
-		log.debug( CF.KHN +"[UserController PostMapping teacherSearchId teacherName]: "+teacherName+ CF.RESET);
-		log.debug( CF.KHN +"[UserController PostMapping teacherSearchId teacherPhone]: "+teacherPhone+ CF.RESET);
-		
-		// teacherId 값 받기/ 서비스 호출
-		String teacherId = userFindService.teacherFindId(teacherName, teacherPhone);
-		log.debug( CF.KHN +"[UserController PostMapping teacherSearchId teacherId]:  "+teacherId+ CF.RESET);		
-		
-		if(teacherId == null) { //teacherId가 널값이라면
-			model.addAttribute("check", 1); //모델값 1로넘김
-		} else {
-			model.addAttribute("check", 0); //모델값 0으로 넘김
-			model.addAttribute("teacherId", teacherId); //studentPw 넘김
-		}
-		return "user/findId";
-	}
-	
-	//운영자ID찾기 get
-	@GetMapping("/user/managerSearchId")
-	public String managerSearchId() {
-		return "user/findId"; //포워딩
-	}
-	//운영자ID찾기 post
-	@PostMapping("/user/managerSearchId")
-	public String managerSearchId(Model model
-			,@RequestParam(name="managerName") String managerName
-			,@RequestParam(name="managerPhone") String managerPhone) {
-		
-		//managerName + managerPhone 값 찾기
-		log.debug( CF.KHN +"[UserController PostMapping managerSearchId managerName]: "+managerName+ CF.RESET);		
-		log.debug( CF.KHN +"[UserController PostMapping managerSearchId managerPhone]: "+managerPhone+ CF.RESET);		
-		
-		//managerId 값 받아옴
-		String managerId = userFindService.managerFindId(managerName, managerPhone);
-		log.debug( CF.KHN +"[UserController PostMapping managerSearchId managerId]: "+managerId+ CF.RESET);		
-		
-		if(managerId == null) { //managerId가 널값이라면
-			model.addAttribute("check", 1);
-		} else {
-			model.addAttribute("check", 0);
-			model.addAttribute("managerId", managerId);
-		}
-		
-		return "user/findId";
 	}
 }
