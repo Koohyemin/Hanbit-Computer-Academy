f0469d3ca4a9a3dd3ddf12bf24ed2b0eb080b558
kyuryang <84187694+kyuryang@users.noreply.github.com>
2022-06-27 13:25:50 +0900

    13:25 [이규량] 회원휴면 스케줄러 시간 변경

--- lms/src/main/java/Hanbit/co/kr/lms/Component/MemberScheduler.java
+++ lms/src/main/java/Hanbit/co/kr/lms/Component/MemberScheduler.java
@@ -14,7 +14,7 @@ public class MemberScheduler {
 	@Autowired
 	MemberService memberService;
 	
-	@Scheduled(cron = "* 23 * * * *")		//정해진 시간대에 실행된다.
+	@Scheduled(cron = "* * 23 * * *")		//정해진 시간대에 실행된다.
 //	@Scheduled(cron = "*/1 * * * * *")
 	public void modifyDormantMember() {
 		//service에서 휴면계정 수정 메서드 호출
